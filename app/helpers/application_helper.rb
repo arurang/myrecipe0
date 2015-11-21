55dd919a228c584a5f171f45b9eff6efacbd4f00
arunc9 <arunc9@outlook.com>
2015-11-21 06:07:13 +0000

    create new edit forms and partials

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def gravator_for(chef, options = {size: 80})
+    gravator_id = Digest::MD5::hexdigest(chef.email.downcase)
+size = options[:size]
+gravator_url = "https://secure.gravatar.com/avatar/#{gravator_id}?s=#{size}"
+image_tag(gravator_url, alt: chef.chefname, class: "gravatar")
+
+end
 end
